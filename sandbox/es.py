4571ff650e45e7be1e430c78237eddb3ff81956d
Bal Balaji <bal.balaji@juliusbaer.com>
2020-10-24 13:33:40 +0200

    es template metrics

---
+++ sandbox/es.py
@@ -0,0 +1,51 @@
+!/usr/bin/env python
+import faust
+import json
+from random import random
+from kafka import KafkaProducer
+import os
+import consul
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+import logging
+from jsonformatter import JsonFormatter
+
+import jinja
+
+print('done')
+
+es = Elasticsearch(hosts='http://elasticsearch:9200',verify_certs=False)
+
+res = es.search(index="ferris-metrics", body={"query": {"match_all": {}}})
+
+print("Got %d Hits:" % res['hits']['total']['value'])
+for hit in res['hits']['hits']:
+    print(hit["_source"])
+    #print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+
+print('done')
+
+
+time_from = "2020-08-28T11:47:09Z"
+time_to = "2020-08-28T11:47:09Z"
+start_from = 0
+result_size = 20
+metric_type = "*.dataloader.*"  
+
+
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = "query_template.jinja"
+template = templateEnv.get_template(TEMPLATE_FILE)
+outputText = template.render(time_from=time_from,time_to=time_to,
+	start_from=start_from,result_size=result_size,metric_type=metric_type) 
+
+print(outputText)
+
+
+
+ 
+                 
+
+                     
