6e09a5f0c152116f57296f3da9f88151ab0616a5
sliche <boriss@ballab.com>
2021-12-20 15:58:57 +0100

    version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ PROJECT_ROOT, _ = os.path.split(__file__)
 NAME = 'ferris-cli'
 EMAILS = 'bal@ballab.com'
 AUTHORS = 'Balaji Bal'
-VERSION = '2.2.1'
+VERSION = '2.2.2'
 
 URL = 'https://github.com/Ferris-Labs/ferris-cli/tree/v2'
 LICENSE = 'Apache2.0'
